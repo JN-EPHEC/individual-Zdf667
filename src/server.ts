1eec1c7f71bc2a43decd353acc6ab20f5eb4bddb
Zdf667 <danielgokalp100@gmail.com>
2026-02-05 15:29:54 +0100

    commit 1

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bienvenue sur mon serveur API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});

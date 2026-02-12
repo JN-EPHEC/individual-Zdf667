b607b62cba922bcebe6154b85ad204c95474f49b
Zdf667 <danielgokalp100@gmail.com>
2026-02-12 14:50:35 +0100

    tp2 ex 2.3

---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+class User extends Model {
+  declare id: number;
+  declare nom: string;
+  declare prenom: string;
+}
+
+User.init(
+  {
+    nom: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    prenom: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+  }
+);
+
+export default User;
